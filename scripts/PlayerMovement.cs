3b2f42895f766c97aa4124dec66db929b1d4503f
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2023-09-15 19:19:17 +0200

    add double jump

--- scripts/PlayerMovement.cs
+++ scripts/PlayerMovement.cs
@@ -2,24 +2,45 @@ using Godot;
 
 public partial class PlayerMovement : CharacterBody2D {
 
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export] public float Speed = 300.0f;
+	[Export] public float JumpVelocity = -400.0f;
+	[Export] public float DoubleJumpVelocity = -200.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	private bool HasDoubleJumped { get; set; }
+	private bool IsInAir { get; set; }
 
 	public override void _PhysicsProcess(double delta) {
 
 		Vector2 velocity = Velocity;
 
+		if (IsInAir) {
+			if (IsOnFloor()) {
+				IsInAir = false;
+				HasDoubleJumped = false;
+			}
+		}
+
 		// Add the gravity.
 		if (!IsOnFloor()) {
-			velocity.Y += Gravity * (float)delta;
+			velocity.Y += gravity * (float)delta;
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
-			velocity.Y = JumpVelocity;
+		if (Input.IsActionJustPressed("ui_accept")) {
+
+			if (IsOnFloor()) {
+				velocity.Y = JumpVelocity;
+				IsInAir = true;
+			}
+
+			if (!IsOnFloor() && !HasDoubleJumped) {
+				velocity.Y = DoubleJumpVelocity;
+				HasDoubleJumped = true;
+			}
+
 		}
 
 		// Get the input direction and handle the movement/deceleration.
