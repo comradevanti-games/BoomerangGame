aa346cb041c328214089ca7e7f0fa09b66ae2c7d
ComradeVanti <kingvanti650@gmail.com>
2023-09-19 16:56:00 +0200

    feat: only shoot when player is close

---
+++ code/PointAtTarget.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Godot;
+
+namespace BoomerangGame;
+
+public partial class PointAtTarget : Node2D
+{
+    private static readonly float quarterRotation = Mathf.DegToRad(90);
+    
+    private Node2D? target;
+
+
+    public void SetTarget(Node2D newTarget)
+    {
+        target = newTarget;
+        PointAt(target);
+    }
+
+    public void ClearTarget()
+    {
+        target = null;
+    }
+
+    private void PointAt(Node2D node)
+    {
+        var direction = node.GlobalPosition - GlobalPosition;
+        Rotation = direction.Angle() + quarterRotation;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (target == null) return;
+
+        PointAt(target);
+    }
+}
