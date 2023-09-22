840232c90b8c57a703944d8fb57b02d5feed3f67
ComradeVanti <kingvanti650@gmail.com>
2023-09-22 20:53:03 +0200

    fix: shoot pause error

---
+++ code/PausableTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace BoomerangGame;
+
+public partial class PausableTimer : Timer
+{
+    public void Pause()
+    {
+        Paused = true;
+    }
+
+    public void Unpause()
+    {
+        Paused = false;
+    }
+}
